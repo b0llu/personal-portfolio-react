471ae9fa9f569907fdb9e65c9752c404c1b7249b
Dhruv <samantdhruv@gmail.com>
2021-12-11 18:44:08 +0530

    styling skill card

---
+++ src/components/data/skills.js
@@ -0,0 +1,56 @@
+export const SkillsData = [
+  {
+    type: "Frontend",
+    list: [
+      {
+        name: "HTML",
+        icon: <i class="devicon-html5-plain-wordmark colored"></i>,
+      },
+      {
+        name: "CSS3",
+        icon: <i class="devicon-css3-plain-wordmark colored"></i>,
+      },
+      {
+        name: "Tailwind CSS",
+        icon: <i class="devicon-tailwindcss-original-wordmark colored"></i>,
+      },
+      {
+        name: "ReactJS",
+        icon: <i class="devicon-react-original colored"></i>,
+      },
+      {
+        name: "NextJS",
+        icon: <i class="devicon-nextjs-line"></i>,
+      },
+      {
+        name: "Javascript",
+        icon: <i class="devicon-javascript-plain colored"></i>,
+      },
+      {
+        name: "Typescript",
+        icon: <i class="devicon-typescript-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Backend",
+    list: [
+      {
+        name: "NodeJS",
+        icon: <i class="devicon-nodejs-plain colored"></i>,
+      },
+      {
+        name: "MongoDB",
+        icon: <i class="devicon-mongodb-plain-wordmark colored"></i>,
+      },
+      {
+        name: "GraphQL",
+        icon: <i class="devicon-graphql-plain-wordmark colored"></i>,
+      },
+      {
+        name: "MySQL",
+        icon: <i class="devicon-mysql-plain-wordmark colored"></i>,
+      },
+    ],
+  },
+];
